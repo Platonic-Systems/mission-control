2f4542208b3454773e0b3637d03a268e24e1ea92
Sridhar Ratnakumar <srid@srid.ca>
2022-12-17 11:40:13 -0500

    use master

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A `flake-parts` module for your Nix devshell scripts";
   inputs = {
-    flake-root.url = "github:srid/flake-root/init";
+    flake-root.url = "github:srid/flake-root";
   };
   outputs = { self, flake-root, ... }: {
     flakeModule = import ./nix/flake-module.nix { inherit flake-root; };
