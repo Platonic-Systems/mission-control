084a11a8d65f8be55ef63b45418dfe8fba6285be
Sridhar Ratnakumar <srid@srid.ca>
2022-12-17 09:48:43 -0500

    use predictable input

--- example/flake.nix
+++ example/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    mission-control.url = "path:../.";
+    mission-control.url = "github:Platonic-Systems/mission-control";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit self; } {
