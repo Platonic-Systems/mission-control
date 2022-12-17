87016fcd06a2e001d265c8285ffdbbcfee012eb1
Sridhar Ratnakumar <srid@srid.ca>
2022-12-17 11:20:05 -0500

    use right branch

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A `flake-parts` module for your Nix devshell scripts";
   inputs = {
-    flake-root.url = "github:srid/flake-root";
+    flake-root.url = "github:srid/flake-root/init";
   };
   outputs = { self, ... }: {
     flakeModule = ./nix/flake-module.nix;
