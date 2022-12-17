1d307f1a4a7cf454865d417c58194113654c952d
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-12-17 10:08:39 -0500

    Add example; test it in CI (#2)

---
+++ example/flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.mission-control.flakeModule
+      ];
+      perSystem = { pkgs, lib, config, ... }: {
+        script.scripts = {
+          hello = {
+            description = "Say Hello";
+            command = "echo Hello";
+          };
+          fmt = {
+            description = "Format the Nix files";
+            command = "${lib.getExe pkgs.nixpkgs-fmt} ./*.nix ../nix/*.nix";
+            category = "Tools";
+          };
+        };
+        devShells.default =
+          let shell = pkgs.mkShell { };
+          in config.script.installToDevShell shell;
+      };
+    };
+}
