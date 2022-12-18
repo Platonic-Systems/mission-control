f3f9af5dbcbda2d75d9d13834f1799e517f93097
Sridhar Ratnakumar <srid@srid.ca>
2022-12-18 13:26:30 -0500

    Use flake-root in example

--- example/flake.nix
+++ example/flake.nix
@@ -2,12 +2,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
     mission-control.url = "github:Platonic-Systems/mission-control";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
+        inputs.mission-control.flake-root
         inputs.mission-control.flakeModule
       ];
       perSystem = { pkgs, lib, config, ... }: {
