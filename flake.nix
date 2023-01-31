fa8a5da6f58c2f412f79f29f290ea126eb61bc11
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-01-31 15:07:10 -0500

    Remove mission-control from example's `flake.lock` (#21)
    Because, in tests, we override it to parent flake anyway. And we do not want the user to accidentally end up using an outdated version of mission-control.
    
    Consequently eliminate the top-level `flake.lock` as well.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,10 @@
 {
   description = "A `flake-parts` module for your Nix devshell scripts";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  };
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, ... }: {
     flakeModule = ./nix/flake-module.nix;
-    templates.default.path = (nixpkgs.lib.cleanSourceWith {
-      src = ./example;
-      filter = path: type: baseNameOf path == "flake.nix";
-    }).outPath;
+    templates.default = {
+      description = "Example flake using mission-control to provide scripts";
+      path =./example;
+    };
   };
 }
